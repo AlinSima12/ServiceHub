5827ef27f95d12c9aebf4ed0a02d667fbd9f473e
AlinSima12 <simaalin81@gmail.com>
2025-02-07 22:08:38 +0200

    added project files

--- frontend/firebaseConfig.ts
+++ frontend/firebaseConfig.ts
@@ -8,15 +8,6 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCSQbRkywhaCR7ssBf_U5KnJ75ppIboOA0",
-  authDomain: "meseriasii-91b95.firebaseapp.com",
-  projectId: "meseriasii-91b95",
-  storageBucket: "meseriasii-91b95.firebasestorage.app",
-  messagingSenderId: "303207656883",
-  appId: "1:303207656883:web:96b682b6f8c3265d906fc6",
-  measurementId: "G-XK8KD5N0XQ"
-};
 
 // Initialize Firebase
 export const firebase_app = initializeApp(firebaseConfig);
